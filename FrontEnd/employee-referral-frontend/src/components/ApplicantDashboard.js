87096b7b78d27977871b7a26121b6114fbddf583
Aryan-Mehta2151 <aryanmehta2151@gmail.com>
2025-09-16 04:18:29 -0400

    Added backend and frontend folders

---
+++ FrontEnd/employee-referral-frontend/src/components/ApplicantDashboard.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ApplicantDashboard = () => {
+  return (
+    <div>
+      <h1>Welcome to the Applicant Dashboard</h1>
+      {/* Add Applicant specific content here */}
+    </div>
+  );
+};
+
+export default ApplicantDashboard;
